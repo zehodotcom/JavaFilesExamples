9c1225e5e60bca2002534386f90df03c22a93049
Angel Garcia <agmartin79@gmail.com>
2022-09-17 11:58:41 +0200

    BufferedReader example added

--- src/main/main.java
+++ src/main/main.java
@@ -1,30 +1,46 @@
 package main;
-
 import java.io.*;
 
+/*
+ * To manipulate files, there are always three steps: 
+ * - Open the file 
+ * - Save data or read data 
+ * - Close the file
+ */
 public class main {
-	/*
-	 To manipulate files, there are always three steps: 
-	 - Open the file 
-	 - Save data or read data 
-	 - Close the file
-	 */
 	public static void main(String[] args) {
-		BufferedWriterExample();
+		WriteFile();
+		ReadFile();
 	}
-	
-	
-	public static void BufferedWriterExample() {
-		System.out.println("Creating a text file...");
+
+	public static void WriteFile() {
+		System.out.println("Creating a text file.");
 		try {
 			BufferedWriter bw = new BufferedWriter(new FileWriter(new File("fileText.txt")));
 			bw.write("Hello");
 			bw.newLine();
 			bw.write("I am a text file");
 			bw.close();
-		} 
-		catch (IOException ex) {
-			System.out.println("Issue: " + ex.getMessage());
+		} catch (IOException ex) {
+			System.out.println("there has been a issue: " + ex.getMessage());
+		}
+	}
+
+	public static void ReadFile() {
+		if (!(new File("fileText.txt")).exists()) {
+			System.out.println("Not found fileText.txt");
+			return;
+		}
+		System.out.println("Reading fileText.txt ...");
+		try {
+			BufferedReader br = new BufferedReader(new FileReader(new File("fileText.txt")));
+			String line = null;
+			while ((line = br.readLine()) != null) {
+				System.out.println(line);
+			}
+			br.close();
+		} catch (IOException ex) {
+			System.out.println("there has been a issue: " + ex.getMessage());
 		}
 	}
 }
